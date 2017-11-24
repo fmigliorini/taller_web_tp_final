0cbe355a2a496af89003770fd7ae482874170ef7
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-23 23:48:50 -0300

    buscar tipo de vehiculo por peso

--- src/main/java/ar/edu/unlam/tallerweb1/dao/TipoVehiculoDao.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/TipoVehiculoDao.java
@@ -2,9 +2,12 @@ package ar.edu.unlam.tallerweb1.dao;
 
 import java.util.List;
 
+import ar.edu.unlam.tallerweb1.modelo.TipoMovimiento;
 import ar.edu.unlam.tallerweb1.modelo.TipoVehiculo;
 
 public interface TipoVehiculoDao {
 	List<TipoVehiculo> listarTiposVehiculos();
 	TipoVehiculo buscarPorId(Long id);
+	TipoVehiculo buscarPorPesoMaximo(Float peso);
+
 }
--- src/main/java/ar/edu/unlam/tallerweb1/dao/TipoVehiculoDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/TipoVehiculoDaoImpl.java
@@ -10,6 +10,7 @@ import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import ar.edu.unlam.tallerweb1.modelo.TipoMovimiento;
 import ar.edu.unlam.tallerweb1.modelo.TipoVehiculo;
 
 @Repository("TipoVehiculoDao")
@@ -17,22 +18,28 @@ public class TipoVehiculoDaoImpl implements TipoVehiculoDao {
 
 	@Inject
 	private SessionFactory sessionFactory;
-	
+
 	@Override
-	public List<TipoVehiculo> listarTiposVehiculos()
-	{
+	public List<TipoVehiculo> listarTiposVehiculos() {
 		final Session session = sessionFactory.getCurrentSession();
 		List<TipoVehiculo> tipoVehiculos = session.createCriteria(TipoVehiculo.class)
-												.list();
+											.list();
 		return tipoVehiculos;
 	}
-	
+
 	@Override
-	public TipoVehiculo buscarPorId(Long id)
-	{
+	public TipoVehiculo buscarPorId(Long id) {
 		final Session session = sessionFactory.getCurrentSession();
 		return (TipoVehiculo) session.createCriteria(TipoVehiculo.class)
 								.add(Restrictions.eq("id", id))
+								.uniqueResult();
+	}
+
+	@Override
+	public TipoVehiculo buscarPorPesoMaximo(Float peso) {
+		final Session session = sessionFactory.getCurrentSession();
+		return (TipoVehiculo ) session.createCriteria(TipoVehiculo.class)
+								.add(Restrictions.ge("pesoMaximo", peso))
 								.uniqueResult();
 	}
 }
