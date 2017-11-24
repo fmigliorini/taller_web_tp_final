f874454faa21939c4bc0672f8e1ffaeca385deaf
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-24 01:21:26 -0300

    Agrego movimiento

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
@@ -1,5 +1,10 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Movimiento;
+import ar.edu.unlam.tallerweb1.modelo.TipoMovimiento;
 import ar.edu.unlam.tallerweb1.modelo.TipoVehiculo;
 import ar.edu.unlam.tallerweb1.modelo.Viaje;
+import ar.edu.unlam.tallerweb1.servicios.ServicioMovimiento;
+import ar.edu.unlam.tallerweb1.servicios.ServicioTipoMovimiento;
 import ar.edu.unlam.tallerweb1.servicios.ServicioTipoVehiculo;
 import ar.edu.unlam.tallerweb1.servicios.ServicioViaje;
 
@@ -24,6 +33,12 @@ public class ControladorPresupuesto {
 	@Inject
 	private ServicioViaje servicioViaje;
 
+	@Inject
+	private ServicioTipoMovimiento servicioTipoMovimiento;
+	
+	@Inject
+	private ServicioMovimiento servicioMovimiento;
+	
 	@RequestMapping("/presupuestoForm")
 	public ModelAndView irAFormularioPresupuesto(HttpServletRequest request) {
 
@@ -52,7 +67,27 @@ public class ControladorPresupuesto {
 		viaje.setPrecio(tv.getPrecio() * viaje.getKilometros());
 		servicioViaje.guardarViaje(viaje);
 
+		
 		// GENERACION DE UN MOVIMIENTO DE TIPO PRESUPUESTO
+		Movimiento movimiento = new Movimiento();
+		
+		// seteo el tipo de movimiento
+		TipoMovimiento presupuesto = servicioTipoMovimiento.buscarPorDescripcion("Factura");
+		movimiento.setTipoMovimiento(presupuesto);
+		
+		// seteo la fecha del presupuesto
+		final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		movimiento.setFecha_hora(sdf.format(date));
+		
+		// Seteo la fecha de vencimiento del presupuesto
+		Calendar c = Calendar.getInstance(); 
+		c.setTime(date);
+		c.add(Calendar.DATE, 1);
+		movimiento.setFecha_vencimiento(sdf.format(c.getTime()));
+		
+		
+		servicioMovimiento.guardarMovimiento(movimiento);
 		
 		ModelMap modeMap = new ModelMap();
 		
