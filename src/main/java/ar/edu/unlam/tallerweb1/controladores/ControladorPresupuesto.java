1fb5e84deef8fda9aa0f0ca60f71106b8f08430b
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-23 23:50:08 -0300

    genero viaje con tipo vehiculo y precio en base a los kilometros y al tipo de vehiculo

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
@@ -8,37 +8,27 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.TipoVehiculo;
 import ar.edu.unlam.tallerweb1.modelo.Viaje;
-import ar.edu.unlam.tallerweb1.modelo.Presupuesto;
-import ar.edu.unlam.tallerweb1.servicios.ServicioPresupuesto;
 import ar.edu.unlam.tallerweb1.servicios.ServicioTipoVehiculo;
-import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioViaje;
 
 @Controller
 public class ControladorPresupuesto {
 
 	@Inject
-	private ServicioUsuario servicioUsuario;
-	
-	@Inject
 	private ServicioTipoVehiculo servicioTipoVehiculo;
-	
+
 	@Inject
-	private ServicioPresupuesto servicioPresupuesto;
-	
-	/**
-	 * Simplemente retorno la vista con el formulario
-	 * para generar un nuevo presupuesto
-	 * @return ModelAndView
-	 */
+	private ServicioViaje servicioViaje;
+
 	@RequestMapping("/presupuestoForm")
 	public ModelAndView irAFormularioPresupuesto(HttpServletRequest request) {
-		
-		Long idUsuario = (Long)request.getSession().getAttribute("idUsuario");
-		if ( idUsuario != null ){
+
+		Long idUsuario = (Long) request.getSession().getAttribute("idUsuario");
+		if (idUsuario != null) {
 			ModelMap model = new ModelMap();
 			model.put("idCliente", idUsuario);
 			model.put("tiposVehiculos", servicioTipoVehiculo.listarTiposVehiculos());
@@ -49,34 +39,24 @@ public class ControladorPresupuesto {
 		return new ModelAndView("redirect:/login");
 
 	}
-	
-	@RequestMapping(path="/generarPresupuesto", method=RequestMethod.POST)
-	public ModelAndView generarPresupuesto(@ModelAttribute("viaje") Viaje viaje)
-	{
-		/*
-	    @RequestParam("idCliente") Long idCliente,
-		@RequestParam("tipoVehiculo") Long idTipoVehiculo,
-		@RequestParam("origen") String origen,
-		@RequestParam("destino") String destino,
-		@RequestParam("kilometros") Integer kilometros,
-		@RequestParam("descripcion") String descripcion
+
+	@RequestMapping(path = "/generarPresupuesto", method = RequestMethod.POST)
+	public ModelAndView generarPresupuesto(@ModelAttribute("viaje") Viaje viaje) {
+
+		// GENERACION DE UN VIAJE
+		TipoVehiculo tv = servicioTipoVehiculo.buscarPorPesoMaximo(viaje.getPeso());
+		if (tv != null) {
+			// No poseemos un vehiculo que permita llevar dicha carga.
+		}
+		viaje.setTipoVehiculo(tv);
+		viaje.setPrecio(tv.getPrecio() * viaje.getKilometros());
+		servicioViaje.guardarViaje(viaje);
+
+		// GENERACION DE UN MOVIMIENTO DE TIPO PRESUPUESTO
 		
-		Presupuesto presupuesto = new Presupuesto();
-		presupuesto.setUsuario(servicioUsuario.buscarPorId(idCliente));
-		presupuesto.setTipoVehiculo(servicioTipoVehiculo.buscarPorId(idTipoVehiculo));		
-		presupuesto.setOrigen(origen);
-		presupuesto.setDestino(destino);
-		presupuesto.setKilometros(kilometros);
-		presupuesto.setDescripcion(descripcion);
-		presupuesto.setPrecio(servicioTipoVehiculo.buscarPorId(idTipoVehiculo).getPrecio() * kilometros);
-		servicioPresupuesto.guardarPresupusto(presupuesto);
+		ModelMap modeMap = new ModelMap();
 		
-		ModelMap model = new ModelMap();
-		model.put("presupuesto", presupuesto);
-		
-		*/
-		
-		return new ModelAndView("invoice");
+		return new ModelAndView("invoice", modeMap);
 	}
 
 }
--- src/main/webapp/WEB-INF/vistas/presupuesto-form.jsp
+++ src/main/webapp/WEB-INF/vistas/presupuesto-form.jsp
@@ -3,9 +3,11 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+
 <div class="container">
 	<h1> Generar presupuesto para nuevo viaje. </h1>
-    <form action="generarPresupuesto" method="POST" modelAttribute="viaje">
+    <form:form action="generarPresupuesto" method="POST" modelAttribute="viaje">
     	<input type="hidden" id="idCliente" name="idCliente" value="${idCliente}">
 	    <div class="form-row">
 		    <div class="form-group col-md-6">
@@ -14,37 +16,37 @@
 		    </div>
 		    <div class="form-group col-md-6">
 		        <label for="destino">Destino</label>
-		        <input type="text" required="required" class="form-control" id="destino" name="destino" placeholder="Calle Verdadera, Isidro Casanova, La Matanza, Buenos Aires">
+		        <form:input type="text" required="required" class="form-control" id="destino" path="destino" placeholder="Calle Verdadera, Isidro Casanova, La Matanza, Buenos Aires" />
 		    </div>
 	    </div>
 	    <div class="form-row">	
 	        <div class="form-group col-md-6">
 	            <label for="fecha">Fecha</label>
-	            <input type="date" required="required" class="form-control" id="fecha" name="fecha">
+	            <form:input type="date" required="required" class="form-control" id="fecha" path="fecha" />
 	        </div>
 	        <div class="form-group col-md-6">
 	            <label for="hora">Hora</label>
-	            <input type="time" required="required" class="form-control" id="hora" name="hora">
+	            <form:input type="time" required="required" class="form-control" id="hora" path="hora" />
 	        </div>
         </div>
 	    <div class="form-row">	
 	        <div class="form-group col-md-12">
 	            <label for="kilometros">Kilometros</label>
-	            <input type="text" required="required" class="form-control" id="kilometros" name="kilometros" readonly>
+	            <form:input type="text" required="required" class="form-control" id="kilometros" path="kilometros" readonly />
 	        </div>
 	        <div class="form-group col-md-12">
 		        <label for="descripcion">peso </label>
-		        <textarea required="required" class="form-control" id="descripcion" name="descripcion" rows="3"></textarea>
+		        <form:input type="text" required="required" class="form-control" id="peso" path="peso" rows="3" />
 		    </div>
 		    <div class="form-group col-md-12">
 		        <label for="descripcion">Descripción </label>
-		        <textarea required="required" class="form-control" id="descripcion" name="descripcion" rows="3"></textarea>
+		        <form:input type="text" required="required" class="form-control" id="descripcion" path="descripcion"  />
 		    </div>
 	    </div>
 	    <input type="submit" class="btn btn-primary" value="Generar Presupuesto">
    	    <button type="submit" class="btn btn-danger">Cancelar</button>
 	    
-	</form>
+	</form:form>
 </div>
 
 <%@include file='../../templates/Footer.jsp' %>
