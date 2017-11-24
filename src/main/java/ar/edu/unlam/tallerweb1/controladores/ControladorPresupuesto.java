8228fb0e824eb7762b70ba8a679b176adcd67967
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-23 23:04:19 -0300

    cambio el controlador a post y recibo un modelo de viaje

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPresupuesto.java
@@ -5,17 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.modelo.Viaje;
 import ar.edu.unlam.tallerweb1.modelo.Presupuesto;
-import ar.edu.unlam.tallerweb1.modelo.TipoVehiculo;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPresupuesto;
 import ar.edu.unlam.tallerweb1.servicios.ServicioTipoVehiculo;
 import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
-
 
 @Controller
 public class ControladorPresupuesto {
@@ -36,25 +36,30 @@ public class ControladorPresupuesto {
 	 */
 	@RequestMapping("/presupuestoForm")
 	public ModelAndView irAFormularioPresupuesto(HttpServletRequest request) {
+		
 		Long idUsuario = (Long)request.getSession().getAttribute("idUsuario");
 		if ( idUsuario != null ){
-		ModelMap model = new ModelMap();
-		model.put("idCliente", idUsuario);
-		model.put("tiposVehiculos", servicioTipoVehiculo.listarTiposVehiculos());
-		return new ModelAndView("presupuesto-form", model);
+			ModelMap model = new ModelMap();
+			model.put("idCliente", idUsuario);
+			model.put("tiposVehiculos", servicioTipoVehiculo.listarTiposVehiculos());
+			Viaje viaje = new Viaje();
+			model.put("viaje", viaje);
+			return new ModelAndView("presupuesto-form", model);
 		}
 		return new ModelAndView("redirect:/login");
 
 	}
 	
-	@RequestMapping("/generarPresupuesto")
-	public ModelAndView generarPresupuesto( @RequestParam("idCliente") Long idCliente,
-											@RequestParam("tipoVehiculo") Long idTipoVehiculo,
-											@RequestParam("origen") String origen,
-											@RequestParam("destino") String destino,
-											@RequestParam("kilometros") Integer kilometros,
-											@RequestParam("descripcion") String descripcion)
+	@RequestMapping(path="/generarPresupuesto", method=RequestMethod.POST)
+	public ModelAndView generarPresupuesto(@ModelAttribute("viaje") Viaje viaje)
 	{
+		/*
+	    @RequestParam("idCliente") Long idCliente,
+		@RequestParam("tipoVehiculo") Long idTipoVehiculo,
+		@RequestParam("origen") String origen,
+		@RequestParam("destino") String destino,
+		@RequestParam("kilometros") Integer kilometros,
+		@RequestParam("descripcion") String descripcion
 		
 		Presupuesto presupuesto = new Presupuesto();
 		presupuesto.setUsuario(servicioUsuario.buscarPorId(idCliente));
@@ -68,7 +73,10 @@ public class ControladorPresupuesto {
 		
 		ModelMap model = new ModelMap();
 		model.put("presupuesto", presupuesto);
-		return new ModelAndView("invoice",model);
+		
+		*/
+		
+		return new ModelAndView("invoice");
 	}
 
 }
