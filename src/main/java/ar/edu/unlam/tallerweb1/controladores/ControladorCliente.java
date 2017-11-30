022625179e32826f82cb00cce21b810e23d64c1f
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-30 14:16:56 -0300

    limpio controlador

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCliente.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCliente.java
@@ -14,17 +14,8 @@ import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
 
 @Controller
-public class ControladorApp {
-
-	/*@RequestMapping(path = "/", method = RequestMethod.GET)
-	public ModelAndView inicio() {
-		return new ModelAndView("redirect:/home");
-	}*/
+public class ControladorCliente {
 	
-	/**
-	 * Retorno la ventana del Home
-	 * @return ModelAndView
-	 */
 	@RequestMapping("/home")
 	public ModelAndView irAFormularioPresupuesto() {
 		return new ModelAndView("home");
