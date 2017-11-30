7101654442ebc44ea67aae0adabfeb04acb36a2c
Erika Romanczuk <ErikaRomanczuk@users.noreply.github.com>
2017-11-30 13:05:59 -0300

    la

--- src/test/java/ar/edu/unlam/tallerweb1/modelo/AdminTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/AdminTest.java
@@ -7,8 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import ar.edu.unlam.tallerweb1.controladores.ControladorLogin;
 import ar.edu.unlam.tallerweb1.controladores.ControladorMenuAdmnistrador;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
 
