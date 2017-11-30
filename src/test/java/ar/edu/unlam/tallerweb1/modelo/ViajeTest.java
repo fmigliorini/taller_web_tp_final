f11fff1e88735db77245eef569660910f501767f
Samanta <samantamansillarubia@gmail.com>
2017-11-30 04:34:36 -0300

    segundo método que testea los viajes terminados, de la clase viaje

--- src/test/java/ar/edu/unlam/tallerweb1/modelo/ViajeTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/modelo/ViajeTest.java
@@ -83,5 +83,73 @@ public class ViajeTest extends SpringTest {
 		}
 
 	}
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void listarViajesTerminados() {
+		
+		Usuario chofer1;
+		Usuario chofer2;
+		Viaje viaje1;
+		Viaje viaje2;
+		TipoVehiculo camion;
+		TipoVehiculo furgoneta;
+		Vehiculo v1;
+		Vehiculo v2;
+		Session session;
+
+		session = getSession();
+		chofer1 = new Usuario();
+		chofer2 = new Usuario();
+		viaje1 = new Viaje();
+		viaje2 = new Viaje();
+		camion = new TipoVehiculo();
+		furgoneta = new TipoVehiculo();
+		v1 = new Vehiculo();
+		v2 = new Vehiculo();
+
+		// Primer viaje
+		chofer1.setRol("chofer");
+		chofer1.setNombre("Pablo");
+		v1.setChofer(chofer1);
+		viaje1.setTipoVehiculo(furgoneta);
+		furgoneta.setDescripcion("furgoneta");
+		viaje1.setVehiculo(v1);
+		viaje1.setEstado("terminado");
+		viaje1.setTipoVehiculo(furgoneta);
+
+		// Segundo viaje
+		chofer2.setRol("chofer");
+		chofer2.setNombre("Mario");
+		v2.setChofer(chofer2);
+		viaje2.setTipoVehiculo(camion);
+		camion.setDescripcion("camion");
+		viaje2.setVehiculo(v2);
+		viaje2.setEstado("terminado");
+		viaje2.setTipoVehiculo(camion);
+
+		// salvamos
+		session.save(chofer1);
+		session.save(furgoneta);
+		session.save(v1);
+		session.save(viaje1);
+
+		session.save(chofer2);
+		session.save(camion);
+		session.save(v2);
+		session.save(viaje2);
+
+		List<Viaje> listarViajesActivos = session.createCriteria(Viaje.class).add(Restrictions.eq("estado", "terminado"))
+				.createAlias("vehiculo", "veh").createAlias("veh.chofer", "ch").add(Restrictions.eq("ch.rol", "chofer"))
+				.list();
+
+		assertThat(listarViajesActivos).hasSize(2);
+		
+		for(Viaje viajesActivos :listarViajesActivos ){
+			assertEquals("chofer",viajesActivos.getVehiculo().getChofer().getRol());
+			assertEquals("terminado",viajesActivos.getEstado());
+			
+		}
+	}
 
 }
