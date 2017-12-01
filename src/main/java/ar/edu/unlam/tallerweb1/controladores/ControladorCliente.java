a0b523899276536d6fe8d5205964cf4c086bf698
Facundo Migliorini <facundo.mig@gmail.com>
2017-12-01 17:26:23 -0300

    agrego seguridad al cliente

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCliente.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorCliente.java
@@ -1,5 +1,7 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,15 +12,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Movimiento;
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
+import ar.edu.unlam.tallerweb1.servicios.ServicioMovimiento;
+import ar.edu.unlam.tallerweb1.servicios.ServicioTipoMovimiento;
 
 @Controller
 public class ControladorCliente {
-	
+
+	@Inject
+	private ServicioMovimiento servicioMovimiento;
+
+	@Inject
+	private ServicioTipoMovimiento servicioTipoMovimiento;
+
 	@RequestMapping("/home")
-	public ModelAndView irAFormularioPresupuesto() {
-		return new ModelAndView("home");
+	public ModelAndView irAFormularioPresupuesto(HttpServletRequest request) {
+		Long idUsuario = (Long) request.getSession().getAttribute("idUsuario");
+		if (idUsuario != null) {
+			return new ModelAndView("home");
+		}
+		return new ModelAndView("redirect:/login");
 	}
 
+	@RequestMapping(path = "/listarPresupuestosCliente")
+	public ModelAndView listarPresupuestosCliente(HttpServletRequest request) {
+		Long idUsuario = (Long) request.getSession().getAttribute("idUsuario");
+		if (idUsuario != null) {
+			List<Movimiento> listaMovimiento = servicioMovimiento.buscarMovimientosPorUsuario(idUsuario,
+					servicioTipoMovimiento.buscarPorDescripcion("Presupuesto").getId());
+			ModelMap model = new ModelMap();
+			model.put("presupuestos", listaMovimiento);
+			return new ModelAndView("cliente-lista-presupuesto", model);
+		}
+		return new ModelAndView("redirect:/login");
+	}
 }
